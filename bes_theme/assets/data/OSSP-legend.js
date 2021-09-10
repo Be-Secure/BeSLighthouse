eb1de8e2a07c9bedfcc10b0074b2a9695537a39f
jobyko <joby.ouseph@wipro.com>
2021-09-10 18:37:56 +0530

    Upload: OSSP-master.js and OSSP-legend.js with initial set of data

---
+++ bes_theme/assets/data/OSSP-legend.js
@@ -0,0 +1,387 @@
+[
+  {
+    "Tracked Open Source Software Projects Master": "",
+    "TOSS-PM": "",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Be-Secure Open Source Security tool stack",
+    "TOSS-PM": "BOS-STS",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "DevOPs Tool",
+    "TOSS-PM": "DO",
+    "": "RGB ( 0, 0, 0)"
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Language & Framework",
+    "TOSS-PM": "L&F",
+    "": "RGB ( 255, 255, 0)"
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Application",
+    "TOSS-PM": "A",
+    "": "RGB ( 255, 0, 0)"
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Distributed & Decentralized Application",
+    "TOSS-PM": "DA",
+    "": "RGB ( 0, 204, 153)"
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Open Source Security Tool",
+    "TOSS-PM": "S",
+    "": "RGB ( 0, 176, 240)"
+  },
+  {
+    "Tracked Open Source Software Projects Master": "",
+    "TOSS-PM": "",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Core Security Theme",
+    "TOSS-PM": "CST",
+    "": "RGB (255, 192, 0)"
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Secure the Cloud",
+    "TOSS-PM": "CST-SC",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Combatting Ransomware",
+    "TOSS-PM": "CST-CR",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Next Gen IAM - Digital Trust",
+    "TOSS-PM": "CST-DT",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Zero Trust",
+    "TOSS-PM": "CST-ZT",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Securing critical infrastructure OT/IoT security",
+    "TOSS-PM": "CST-OTS",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Managing Third Party/ Supply Chain Risk",
+    "TOSS-PM": "CST-TPR",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "",
+    "TOSS-PM": "",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Security Domain",
+    "TOSS-PM": "SD",
+    "": "RGB (0, 176, 80)"
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Security Risk Management",
+    "TOSS-PM": "SD-SRM",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Application Security",
+    "TOSS-PM": "SD-AS",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Data Security",
+    "TOSS-PM": "SD-DS",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Infrastructure Security",
+    "TOSS-PM": "SD-IS",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Security Operations",
+    "TOSS-PM": "SD-SOC",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Identity Access Management",
+    "TOSS-PM": "SD-IAM",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Cloud Security",
+    "TOSS-PM": "SD-CS",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Blockchain Security",
+    "TOSS-PM": "SD-BS",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Network Security",
+    "TOSS-PM": "SD-NS",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "IoT/OT Security",
+    "TOSS-PM": "SD-IoTS",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "",
+    "TOSS-PM": "",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Technology Domain (Usage)",
+    "TOSS-PM": "TD-U",
+    "": "RGB (0, 112, 192)"
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Web Development",
+    "TOSS-PM": "TD-U-W",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Artifical Intelligence / Machine Learning",
+    "TOSS-PM": "TD-U-AI/ML",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Database",
+    "TOSS-PM": "TD-U-Db",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Messaging",
+    "TOSS-PM": "TD-U-Msg",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "API Development",
+    "TOSS-PM": "TD-U-ApD",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Mobile Application",
+    "TOSS-PM": "TD-U-MoA",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Desktop Application",
+    "TOSS-PM": "TD-U-DkA",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Data Analytics",
+    "TOSS-PM": "TD-U-DAA",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "IoT",
+    "TOSS-PM": "TD-U-IoT",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Blockchain",
+    "TOSS-PM": "TD-U-Bln",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "",
+    "TOSS-PM": "",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Industry",
+    "TOSS-PM": "IND",
+    "": "RGB (112, 48, 160)"
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Banking & Financial Institutions",
+    "TOSS-PM": "BFSI",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Healthcare & Life Scienses",
+    "TOSS-PM": "HLS",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Consumer",
+    "TOSS-PM": "CON",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Retail",
+    "TOSS-PM": "RT",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Agritech",
+    "TOSS-PM": "AgT",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Education",
+    "TOSS-PM": "ED",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Non Profit & Govt",
+    "TOSS-PM": "NG",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Energy & Utilities",
+    "TOSS-PM": "ENU",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "All",
+    "TOSS-PM": "ALL",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Automotive",
+    "TOSS-PM": "AM",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Telecom",
+    "TOSS-PM": "TEL",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Cybersecurity",
+    "TOSS-PM": "CyS",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "",
+    "TOSS-PM": "",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Technology Domain Composition",
+    "TOSS-PM": "TD-C",
+    "": "RGB (45,100, 85)"
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Client Application",
+    "TOSS-PM": "TD-C-CA",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Server",
+    "TOSS-PM": "TD-C-S",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Web Application",
+    "TOSS-PM": "TD-C-WA",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "API clients",
+    "TOSS-PM": "TD-C-A",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Database",
+    "TOSS-PM": "TD-C-D",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Messaging",
+    "TOSS-PM": "TD-C-M",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Distributed Application",
+    "TOSS-PM": "TD-C-DA",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Microservices",
+    "TOSS-PM": "TD-C-Ms",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Container",
+    "TOSS-PM": "TD-C-C",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Wallet App",
+    "TOSS-PM": "TD-C-Wlt",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "",
+    "TOSS-PM": "",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Open Source project type",
+    "TOSS-PM": "COM",
+    "": "RGB(105,195,215 )"
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Foundation led projects",
+    "TOSS-PM": "",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "OCA",
+    "TOSS-PM": "",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "OpenSSF",
+    "TOSS-PM": "",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "CNCF",
+    "TOSS-PM": "",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "FINOS",
+    "TOSS-PM": "",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Hyperledger",
+    "TOSS-PM": "",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "ONF",
+    "TOSS-PM": "",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "ToIP",
+    "TOSS-PM": "",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "OMF",
+    "TOSS-PM": "",
+    "": ""
+  },
+  {
+    "Tracked Open Source Software Projects Master": "Community led projects",
+    "TOSS-PM": "",
+    "": ""
+  }
+]
