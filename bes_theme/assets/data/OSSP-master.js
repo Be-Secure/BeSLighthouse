1bf7bb6e04356155d9cc5529d7f2e77b63127805
jobyko <joby.ouseph@wipro.com>
2021-09-21 09:13:02 +0530

    [Update] OSSP-master.js updated with new set of projects

--- bes_theme/assets/data/OSSP-master.js
+++ bes_theme/assets/data/OSSP-master.js
@@ -1,5 +1,5 @@
 modelDataAvailable({
-    "total_count": 2,
+    "total_count": 7,
     "incomplete_results": false,
     "items":
 [
@@ -31,13 +31,8 @@ modelDataAvailable({
                 "name": "Apache License 2.0",
                 "spdx_id": "Apache-2.0",
                 "url": "https://api.github.com/licenses/apache-2.0",
-                "node_id": ""
-            },
-            "BSD": {
-                "key": "BSD",
-                "name": "BSD",
-                "spdx_id": "BSD"
-            },
+                "node_id": "MDc6TGljZW5zZTI="
+            }
 
         },
         "language": [
@@ -82,21 +77,16 @@ modelDataAvailable({
 		"main_github_url": "https://github.com/WordPress/WordPress",
 		"main_bes_url": "https://github.com/Be-Secure/WordPress",
 		"all_projects": {
-			"main_project_wallabag": "https://github.com/WordPress/WordPress"
+			"main_project_wordpress": "https://github.com/WordPress/WordPress"
 		},
 
 		"licenses": {
-			"apache-2.0": {
+			"gpl-2.0": {
 				"key": "gpl-2.0",
 				"name": "GNU General Public License v2.0",
 				"spdx_id": "GPL-2.0",
 				"url": "https://api.github.com/licenses/gpl-2.0",
 				"node_id": "MDc6TGljZW5zZTg="
-			},
-			"BSD": {
-				"key": "BSD",
-				"name": "BSD",
-				"spdx_id": "BSD"
 			}
 
 		},
@@ -120,6 +110,262 @@ modelDataAvailable({
 			]
 		}
 	}
+},
+
+{
+    "id": 69,
+    "bes_tracking_id": 69,
+    "name": "Molecule",
+    "description": "Molecule project is designed to aid in the development and testing of Ansible roles.Molecule provides support for testing with multiple instances, operating systems and distributions, virtualization providers, test frameworks and testing scenarios.",
+    "cvedetails": {
+        "bes_cve_details_id": "",
+        "cvedetails_product_id": "",
+        "cvedetails_vendor_id": ""
+    },
+    "bes_tech_stack": {
+        "type": "DO",
+        "sub_category": "Client application"
+    },
+    "project_repos": {
+        "main_github_url": "https://github.com/ansible-community/molecule",
+        "main_bes_url": "https://github.com/Be-Secure/molecule",
+        "all_projects": {
+            "main_project_molekule": "https://github.com/Be-Secure/molecule"
+        },
+
+        "licenses": {
+            "apache-2.0": {
+                "key": "apache-2.0",
+                "name": "Apache License 2.0",
+                "spdx_id": "Apache-2.0",
+                "url": "https://api.github.com/licenses/apache-2.0",
+                "node_id": "MDc6TGljZW5zZTI="
+            }
+        },
+        "language": [
+            "go"
+        ],
+        "tags": {
+
+            "IND": [
+                "ALL"
+            ],
+            "COM": {
+                "organisation": "ansible-community"
+            },
+            "TD-C": [
+                "TD-C-CA"
+            ],
+            "TD-U": [
+                "TD-U-DAA"
+            ]
+        }
+    }
+},
+
+{
+    "id": 68,
+    "bes_tracking_id": 68,
+    "name": "helm",
+    "description": "Helm is a tool for managing Charts. Charts are packages of pre-configured Kubernetes resources",
+    "cvedetails": {
+        "bes_cve_details_id": "",
+        "cvedetails_product_id": "57140",
+        "cvedetails_vendor_id": "20129"
+    },
+    "bes_tech_stack": {
+        "type": "DO",
+        "sub_category": "Client application"
+    },
+    "project_repos": {
+        "main_github_url": "https://github.com/helm/helm",
+        "main_bes_url": "https://github.com/be-secure/helm",
+        "all_projects": {
+            "main_project_helm": "https://github.com/Be-Secure/molecule"
+        },
+
+        "licenses": {
+            "apache-2.0": {
+                "key": "apache-2.0",
+                "name": "Apache License 2.0",
+                "spdx_id": "Apache-2.0",
+                "url": "https://api.github.com/licenses/apache-2.0",
+                "node_id": "MDc6TGljZW5zZTI="
+            }
+        },
+        "language": [
+            "Go"
+        ],
+        "tags": {
+
+            "IND": [
+                "ALL"
+            ],
+            "COM": {
+                "organisation": "helm"
+            },
+            "TD-C": [
+                "TD-C-CA"
+            ],
+            "TD-U": [
+                "TD-U-DAA"
+            ]
+        }
+    }
+},
+
+{
+    "id": 67,
+    "bes_tracking_id": 67,
+    "name": "minikube",
+    "description": "Minikube implements a local Kubernetes cluster on macOS, Linux, and Windows. Minikube's primary goals are to be the best tool for local Kubernetes application development and to support A Kubernetes features that fit",
+    "cvedetails": {
+        "bes_cve_details_id": "",
+        "cvedetails_product_id": "61354",
+        "cvedetails_vendor_id": "15867"
+    },
+    "bes_tech_stack": {
+        "type": "DO",
+        "sub_category": "Client application"
+    },
+    "project_repos": {
+        "main_github_url": "https://github.com/kubernetes/minikube",
+        "main_bes_url": "https://github.com/be-secure/minikube",
+        "all_projects": {
+            "main_project_minikube": "https://github.com/be-secure/minikube"
+        },
+
+        "licenses": {
+            "apache-2.0": {
+                "key": "apache-2.0",
+                "name": "Apache License 2.0",
+                "spdx_id": "Apache-2.0",
+                "url": "https://api.github.com/licenses/apache-2.0",
+                "node_id": "MDc6TGljZW5zZTI="
+            }
+        },
+        "language": [
+            "Go"
+        ],
+        "tags": {
+
+            "IND": [
+                "ALL"
+            ],
+            "COM": {
+                "organisation": "kubernetes"
+            },
+            "TD-C": [
+                ""
+            ],
+            "TD-U": [
+                "TD-C-C"
+            ]
+        }
+    }
+},
+
+{
+    "id": 67,
+    "bes_tracking_id": 67,
+    "name": "secondmate",
+    "description": "An open-source, mini imitation of GitHub Copilot for Emacs",
+    "cvedetails": {
+        "bes_cve_details_id": "",
+        "cvedetails_product_id": "",
+        "cvedetails_vendor_id": ""
+    },
+    "bes_tech_stack": {
+        "type": "DO",
+        "sub_category": "Client application"
+    },
+    "project_repos": {
+        "main_github_url": "https://github.com/be-secure/emacs-secondmate",
+        "main_bes_url": "https://github.com/samrawal/emacs-secondmate",
+        "all_projects": {
+            "main_project_minikube": "https://github.com/be-secure/emacs-secondmate"
+        },
+
+        "licenses": {
+            "apache-2.0": {
+                "key": "apache-2.0",
+                "name": "Apache License 2.0",
+                "spdx_id": "Apache-2.0",
+                "url": "https://api.github.com/licenses/apache-2.0",
+                "node_id": "MDc6TGljZW5zZTI="
+            }
+
+        },
+        "language": [
+            "Go"
+        ],
+        "tags": {
+
+            "IND": [
+                "ALL"
+            ],
+            "COM": {
+                "organisation": "samrawal"
+            },
+            "TD-C": [
+                "TD-C-CA"
+            ],
+            "TD-U-W": [
+                "TD-C-C"
+            ]
+        }
+    }
+},
+
+{
+    "id": 43,
+    "bes_tracking_id": 43,
+    "name": "hexo",
+    "description": "A fast, simple & powerful blog framework, powered by Node.js",
+    "cvedetails": {
+        "bes_cve_details_id": "",
+        "cvedetails_product_id": "",
+        "cvedetails_vendor_id": ""
+    },
+    "bes_tech_stack": {
+        "type": "A",
+        "sub_category": "Web application"
+    },
+    "project_repos": {
+        "main_github_url": "https://github.com/hexojs/hexo",
+        "main_bes_url": "https://github.com/Be-Secure/hexo",
+        "all_projects": {
+            "main_project_minikube": "https://github.com/hexojs/hexo"
+        },
+
+        "licenses": {
+            "apache-2.0": {
+                "key": "apache-2.0",
+                "name": "Apache License 2.0",
+                "spdx_id": "Apache-2.0",
+                "url": "https://api.github.com/licenses/apache-2.0",
+                "node_id": "MDc6TGljZW5zZTI="
+            }
+        },
+        "language": [
+            "Javascript"
+        ],
+        "tags": {
+
+            "IND": [
+                "ALL"
+            ],
+            "COM": {
+                "organisation": "hexojs"
+            },
+            "TD-C": [
+                "TD-C-WA"
+            ],
+            "TD-U-W": [
+                "TD-U-W"
+            ]
+        }
+    }
 }
 
 ]
