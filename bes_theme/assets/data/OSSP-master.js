d899182b487a361aeb047b0cd8aab7f40794c1c5
asa1997 <arun.suresh@wipro.com>
2021-09-22 15:18:27 +0530

    Added prometheus

---
+++ bes_theme/assets/data/OSSP-master.js
@@ -0,0 +1,63 @@
+[
+    {
+        "id": 50,
+        "bes_tracking_id": 50,
+        "name": "prometheus",
+        "description": "The Prometheus monitoring system and time series database.",
+        "cvedetails": {                                                  
+            "bes_cve_details_id": "",                      
+            "cvedetails_product_id": "61503",                      
+            "cvedetails_vendor_id": "20905"
+        }, 
+        "project_repos": {
+            "main_github_url": "https://github.com/prometheus/prometheus",
+            "main_bes_url": "https://github.com/Be-Secure/prometheus",
+            "all_projects": {                                                                         
+                "main_project_xxx" : "",
+                "sub_project_xxx":""
+            },
+            "all_bes_repos": {                                                                      
+                 "sub_project_xxx":""
+           }   
+        },
+        "license": {
+            "key": "apache-2.0",
+            "name": "Apache License 2.0",
+            "spdx_id": "Apache-2.0",
+            "url": "https://api.github.com/licenses/apache-2.0",
+            "node_id": "MDc6TGljZW5zZTI="
+          },   
+        "language": [
+
+            "Go",
+            "TypeScript",
+            "JavaScript",
+            "HTML",
+            "Yacc",
+            "SCSS",
+            "CSS",
+            "Shell",
+            "Lex",
+            "Makefile",
+            "Dockerfile"
+
+        ],
+        "tags": {
+            "IND": [
+                "ALL"
+            ],
+            "SD": [
+                "SD-SOC"
+            ],
+            "COM": {
+                "foundation": "CNCF"
+            },
+            "TD-U": [
+                "TD-U-DAA"
+            ]
+
+        }
+            
+          
+    }
+]
