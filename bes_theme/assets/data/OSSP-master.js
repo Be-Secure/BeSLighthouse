987417e3760433217f51a5a0ef3df773cea36808
asa1997 <arun.suresh@wipro.com>
2021-09-22 16:45:13 +0530

    Added details of defectdojo

--- bes_theme/assets/data/OSSP-master.js
+++ bes_theme/assets/data/OSSP-master.js
@@ -60,5 +60,64 @@
         }
             
           
+    },
+    {
+        "id": 57,
+        "github_id": 382212966,
+        "bes_tracking_id": 57,
+        "name": "django-DefectDojo",
+        "description": "DefectDojo is an open-source application vulnerability correlation and security orchestration tool.",
+        "cvedetails": {                                                  
+            "bes_cve_details_id": "",                      
+            "cvedetails_product_id": "",                      
+            "cvedetails_vendor_id": ""
+        }, 
+        "project_repos": {
+            "main_github_url": "https://github.com/DefectDojo/django-DefectDojo",
+            "main_bes_url": "https://github.com/Be-Secure/django-DefectDojo",
+            "all_projects": {                                                                         
+                "main_project_xxx" : "",
+                "sub_project_xxx":""
+            },
+            "all_bes_repos": {                                                                      
+                 "sub_project_xxx":""
+           }   
+        },
+        "license": {
+            "key": "bsd-3-clause",
+            "name": "BSD 3-Clause \"New\" or \"Revised\" License",
+            "spdx_id": "BSD-3-Clause",
+            "url": "https://api.github.com/licenses/bsd-3-clause",
+            "node_id": "MDc6TGljZW5zZTU="
+          },
+        "language": [
+
+            "HTML",
+            "Python",
+            "JavaScript",
+            "Smarty",
+            "Shell",
+            "CSS",
+            "Mustache",
+            "Batchfile"
+
+        ],
+        "tags": {
+            "IND": [
+                "ALL"
+            ],
+            "SD": [
+                "SD-SOC"
+            ],
+            "COM": {
+                "Community led": "OWASP"
+            },
+            "TD-U": [
+                "TD-U-Msg"
+            ]
+
+        }
+            
+          
     }
 ]
