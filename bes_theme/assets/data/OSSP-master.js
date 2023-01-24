67ef1bd6136d8b1f1d36f4749e77457e339b3e01
asa1997 <arunsureshampadath@gmail.com>
2023-01-24 16:31:09 +0530

    Added fastjson

--- bes_theme/assets/data/OSSP-master.js
+++ bes_theme/assets/data/OSSP-master.js
@@ -64,7 +64,78 @@ modelDataAvailable({
                     "TD-U-W"
                 ]
             }
-        }
+        },
+        {
+			"id": 130,
+			"bes_tracking_id": 130,
+			"name": "fastjson",
+			"full_name": "Be-Secure/fastjson",
+			"description": "A fast JSON parser/generator for Java.  ",
+			"watchers_count": 0,
+			"forks_count": 0,
+			"stargazers_count": 0,
+			"size": 15449,
+			"open_issues": 0,
+			"created_at": "2022-06-01T08:40:32Z",
+			"updated_at": "2022-06-20T05:16:20Z",
+			"pushed_at": "2022-06-20T05:16:11Z",
+			"git_url": "git://github.com/Be-Secure/fastjson.git",
+			"clone_url": "https://github.com/Be-Secure/fastjson.git",
+			"html_url": "https://github.com/Be-Secure/fastjson",
+			"homepage": "https://github.com/alibaba/fastjson/wiki",
+			"owner": {
+				"login": "Be-Secure",
+				"id": 44028837,
+				"avatar_url": "https://avatars.githubusercontent.com/u/44028837?v=4",
+				"html_url": "https://github.com/Be-Secure"
+			},
+            "bes_technology_stack": "https://raw.githubusercontent.com/Be-Secure/Be-Secure/master/docs/assets/images/L%26F_stack.png",
+			"project_repos": {
+				"main_github_url": "https://github.com/alibaba/fastjson",
+				"main_bes_url": "https://github.com/Be-Secure/fastjson",
+				"all_projects": {
+					"fastjson": "https://github.com/alibaba/fastjson"
+
+				},
+				"all_bes_repos": [{
+						"id": 498655991,
+						"name": "fastjson",
+						"url": "https://github.com/Be-Secure/fastjson"
+					}
+
+				]
+			},
+			"license": {
+				"key": "apache-2.0",
+				"name": "Apache License 2.0",
+				"spdx_id": "Apache-2.0",
+				"url": "https://api.github.com/licenses/apache-2.0",
+				"node_id": "MDc6TGljZW5zZTI="
+			},
+			"language": [
+
+				"Java"
+
+			],
+
+			"tags": {
+				"IND": [
+					"ALL"
+				],
+				"SD": [
+					""
+				],
+				"COM": [
+					"Alibaba"
+				],
+				"TD-U": [
+					"TD-U-W"
+				]
+
+			}
+
+
+		},
 
     ]
 })
