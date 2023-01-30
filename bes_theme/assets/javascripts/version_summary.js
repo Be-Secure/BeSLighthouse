6beca5024131e67820c353de8fb6da2ae8960b0f
asa1997 <arunsureshampadath@gmail.com>
2023-01-30 12:10:53 +0530

    Added libraries for graphs

--- bes_theme/assets/javascripts/version_summary.js
+++ bes_theme/assets/javascripts/version_summary.js
@@ -1,3 +1,123 @@
+function load_version_data()
+{
+    console.log("called");
+    id = localStorage["id"]
+    console.log("id:"+id);
+    ossp_name = localStorage["name"];
+    console.log("name:"+ossp_name);
+
+  fetch('../assets/data/version_details/'+id+'-'+ossp_name+'-Versiondetails.json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    
+    console.log(data);
+    console.log("length:"+data.length);
+    console.log("version:"+data[0].version);
+    document.getElementById("heading1").innerHTML = "Version Details - " + ossp_name;
+
+    // For each version of the project, we are creating different tags on the fly.
+    for (let i = 0; i<Object.keys(data).length; i++)
+    {
+      
+      const version_table = document.createElement("TABLE");
+      version_table.setAttribute("id", "version_table"+i);
+
+      const version = document.createElement("p");
+      const release_date = document.createElement("p");
+      const criticality = document.createElement("p");
+      const scorecard = document.createElement("p");
+      const cve_table = document.createElement("TABLE");
+      const scorecard_button = document.createElement("BUTTON") 
+      const codeql_button = document.createElement("BUTTON") 
+      const sonarqube_button = document.createElement("BUTTON");
+      const div_tag_bar1 = document.createElement("div")
+      const div_tag_bar2 = document.createElement("div")
+      const line_graph = document.createElement("div");
+
+      cve_table.setAttribute("id", "cve_table"+i) // Setting id for each cve table. i is the loop var so each table would have different ids. Hence, we can create different tables for each cve details.
+      cve_table.setAttribute("class", "table")
+      div_tag_bar1.setAttribute("id", "bar_chart_vuln_by_type")
+      div_tag_bar2.setAttribute("id", "bar_chart_vuln_by_year")
+      line_graph.setAttribute("id", "line_chart");
+      div_tag_bar1.setAttribute("class", "graph");
+      div_tag_bar2.setAttribute("class", "graph");
+      line_graph.setAttribute("class", "graph");
+      
+      const version_data = document.createTextNode("Version:"+data[i].version);
+      const release_date_data = document.createTextNode("Release date:"+data[i].release_date);
+      const criticality_data = document.createTextNode("Criticality Score:"+data[i].criticality_score);
+      const scorecard_data = document.createTextNode("Scorecard:"+data[i].scorecard);
+      const scorecard_button_text = document.createTextNode("Scorecard");
+      const codeql_button_text = document.createTextNode("Codeql");
+      const sonarqube_button_text = document.createTextNode("Sonarqube")
+      
+      scorecard_button.setAttribute("id", data[i].version);
+      scorecard_button.setAttribute("name", "scorecard")
+      codeql_button.setAttribute("id", data[i].version);
+      codeql_button.setAttribute("name", "codeql");
+      sonarqube_button.setAttribute("id", data[i].version);
+      sonarqube_button.setAttribute("name", "sonarqube")
+
+      version.appendChild(version_data);
+      release_date.appendChild(release_date_data);
+      criticality.appendChild(criticality_data);      
+      scorecard.appendChild(scorecard_data);    
+      scorecard_button.appendChild(scorecard_button_text);  
+      codeql_button.appendChild(codeql_button_text);  
+      sonarqube_button.appendChild(sonarqube_button_text);  
+
+      
+      const element = document.getElementById("version_details");
+      
+
+      element.appendChild(version_table);
+      let table = "<tr><td><h3>Version : " + data[i].version + "</h3></td></tr>"
+      table += "<tr><td>Release date : " + data[i].release_date + "</td></tr>"
+      table += "<tr><td>Criticality Score : " + data[i].criticality_score + "</td></tr>"
+      table += "<tr><td>Scorecard : " + data[i].scorecard + "</td></tr>"
+      document.getElementById("version_table"+i).innerHTML = table;
+      
+      // All the above created elements are added into div tag with id version_details. 
+      element.appendChild(div_tag_bar1);
+      element.appendChild(div_tag_bar2);
+      // element.appendChild(line_graph);
+      element.appendChild(scorecard_button);
+      element.appendChild(codeql_button);
+      element.appendChild(sonarqube_button);
+      
+      bar_chart_by_type(data[i].cve_details);
+      bar_chart_by_year(data[i].cve_details);
+      // line_graph(data[i].cve_details);
+
+
+
+      const buttons = document.getElementsByTagName("button");
+
+      const buttonPressed = e => {
+        console.log(e.target.id);  // Get ID of Clicked Element
+        console.log(e.target.name);
+        var version = e.target.id;
+        var report = e.target.name;
+        open_report(version, report, localStorage["name"]);
+      }
+
+      for (let b of buttons) {
+        b.addEventListener("click", buttonPressed);
+      } 
+      
+    }
+  
+  })
+  
+  .catch(function (err) {
+  
+    console.dir(err);
+  
+  });
+
+}
 
 function bar_chart_by_year(data)
 {
@@ -296,123 +416,6 @@ function open_report(version, report, project_name)
   window.open("../../bes_assessment_reports", "_self");
 }
 
-function load_version_data()
-{
-    console.log("called");
-    id = localStorage["id"]
-    console.log("id:"+id);
-    ossp_name = localStorage["name"];
-    console.log("name:"+ossp_name);
-
-  fetch('../assets/data/version_details/'+id+'-'+ossp_name+'-Versiondetails.json')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    
-    console.log(data);
-    console.log("length:"+data.length);
-    console.log("version:"+data[0].version);
-    document.getElementById("heading1").innerHTML = "Version Details - " + ossp_name;
-
-    // For each version of the project, we are creating different tags on the fly.
-    for (let i = 0; i<Object.keys(data).length; i++)
-    {
-      
-      const version_table = document.createElement("TABLE");
-      version_table.setAttribute("id", "version_table"+i);
-
-      const version = document.createElement("p");
-      const release_date = document.createElement("p");
-      const criticality = document.createElement("p");
-      const scorecard = document.createElement("p");
-      const cve_table = document.createElement("TABLE");
-      const scorecard_button = document.createElement("BUTTON") 
-      const codeql_button = document.createElement("BUTTON") 
-      const sonarqube_button = document.createElement("BUTTON");
-      const div_tag_bar1 = document.createElement("div")
-      const div_tag_bar2 = document.createElement("div")
-      const line_graph = document.createElement("div");
-
-      cve_table.setAttribute("id", "cve_table"+i) // Setting id for each cve table. i is the loop var so each table would have different ids. Hence, we can create different tables for each cve details.
-      cve_table.setAttribute("class", "table")
-      div_tag_bar1.setAttribute("id", "bar_chart_vuln_by_type")
-      div_tag_bar2.setAttribute("id", "bar_chart_vuln_by_year")
-      line_graph.setAttribute("id", "line_chart");
-      
-      const version_data = document.createTextNode("Version:"+data[i].version);
-      const release_date_data = document.createTextNode("Release date:"+data[i].release_date);
-      const criticality_data = document.createTextNode("Criticality Score:"+data[i].criticality_score);
-      const scorecard_data = document.createTextNode("Scorecard:"+data[i].scorecard);
-      const scorecard_button_text = document.createTextNode("Scorecard");
-      const codeql_button_text = document.createTextNode("Codeql");
-      const sonarqube_button_text = document.createTextNode("Sonarqube")
-      
-      scorecard_button.setAttribute("id", data[i].version);
-      scorecard_button.setAttribute("name", "scorecard")
-      codeql_button.setAttribute("id", data[i].version);
-      codeql_button.setAttribute("name", "codeql");
-      sonarqube_button.setAttribute("id", data[i].version);
-      sonarqube_button.setAttribute("name", "sonarqube")
-
-      version.appendChild(version_data);
-      release_date.appendChild(release_date_data);
-      criticality.appendChild(criticality_data);      
-      scorecard.appendChild(scorecard_data);    
-      scorecard_button.appendChild(scorecard_button_text);  
-      codeql_button.appendChild(codeql_button_text);  
-      sonarqube_button.appendChild(sonarqube_button_text);  
-
-      
-      const element = document.getElementById("version_details");
-      
-
-      element.appendChild(version_table);
-      let table = "<tr><td><h3>Version : " + data[i].version + "</h3></td></tr>"
-      table += "<tr><td>Release date : " + data[i].release_date + "</td></tr>"
-      table += "<tr><td>Criticality Score : " + data[i].criticality_score + "</td></tr>"
-      table += "<tr><td>Scorecard : " + data[i].scorecard + "</td></tr>"
-      document.getElementById("version_table"+i).innerHTML = table;
-      
-      // All the above created elements are added into div tag with id version_details. 
-      element.appendChild(div_tag_bar1);
-      element.appendChild(div_tag_bar2);
-      element.appendChild(line_graph);
-      element.appendChild(scorecard_button);
-      element.appendChild(codeql_button);
-      element.appendChild(sonarqube_button);
-      
-      bar_chart_by_type(data[i].cve_details);
-      bar_chart_by_year(data[i].cve_details);
-      // line_graph(data[i].cve_details);
-
-
-
-      const buttons = document.getElementsByTagName("button");
-
-      const buttonPressed = e => {
-        console.log(e.target.id);  // Get ID of Clicked Element
-        console.log(e.target.name);
-        var version = e.target.id;
-        var report = e.target.name;
-        open_report(version, report, localStorage["name"]);
-      }
-
-      for (let b of buttons) {
-        b.addEventListener("click", buttonPressed);
-      } 
-      
-    }
-  
-  })
-  
-  .catch(function (err) {
-  
-    console.dir(err);
-  
-  });
-
-}
 
 function constructTable(selector, list) {
 
