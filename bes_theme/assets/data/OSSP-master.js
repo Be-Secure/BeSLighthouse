13ba793b0dc9cb51e53ff5bbc4023a8c0ce17d13
Vidyaaa24 <vidyadevi.salunkhe@wipro.com>
2023-02-24 23:39:57 +0530

    added all5

--- bes_theme/assets/data/OSSP-master.js
+++ bes_theme/assets/data/OSSP-master.js
@@ -1467,6 +1467,610 @@ const projectOfInterestObject = {
 
 		},
 		{
+			"id": 128,
+			"bes_tracking_id": 152,
+			"issue_url": "https://github.com/Be-Secure/BeSLighthouse/issues/152",
+			"name": "joplin",
+			"full_name": "Be-Secure/joplin",
+			"description": "Joplin - an open source note taking and to-do application with synchronization capabilities for Windows, macOS, Linux, Android and iOS. Forum: https://discourse.joplinapp.org/",
+			"watchers_count": 0,
+			"forks_count": 0,
+			"stargazers_count": 0,
+			"size": 215192,
+			"open_issues": 0,
+			"created_at": "2017-01-16T21:49:41Z",
+    		"updated_at": "2023-02-23T18:59:10Z",
+    		"pushed_at": "2023-02-23T16:42:53Z",
+			"git_url": "git://github.com/Be-Secure/joplin.git",
+			"clone_url": "https://github.com/Be-Secure/joplin.git",
+			"html_url": "https://github.com/Be-Secure/joplin",
+			"homepage": "https://joplinapp.org",
+			"owner": {
+				"login": "Be-Secure",
+				"id": 44028837,
+				"avatar_url": "https://avatars.githubusercontent.com/u/44028837?v=4",
+				"html_url": "https://github.com/Be-Secure"
+			},
+            "bes_technology_stack": "A",
+			"project_repos": {
+				"main_github_url": "https://github.com/laurent22/joplin",
+				"main_bes_url": "https://github.com/Be-Secure/joplin",
+				"all_projects": {
+					"joplin": "https://github.com/laurent22/joplin"
+
+				},
+				"all_bes_repos": [{
+						"id": 79162682,
+						"name": "joplin",
+						"url": "https://github.com/Be-Secure/joplin"
+					}
+
+				]
+			},
+			"license": {
+				"key": "other",
+				"name": "Other",
+				"spdx_id": "NOASSERTION",
+				"url": null,
+				"node_id": "MDc6TGljZW5zZTA="
+			},
+			"language": [
+				{
+					"TypeScript": 3501550,
+					"JavaScript": 2959974,
+					"HTML": 130183,
+					"CSS": 101881,
+					"Mustache": 48814,
+					"Handlebars": 31635,
+					"Shell": 22781,
+					"Java": 20784,
+					"Objective-C": 16633,
+					"Ruby": 2194,
+					"Starlark": 1920,
+					"C": 419,
+					"Batchfile": 140
+				}
+			],
+
+			"tags": [
+				"A",
+				"IND-ALL",
+				"COM - C",
+				"TD-U-W",
+				"TD-U-DkA",
+				"TD-U-MoA",
+			   
+			]
+
+		},
+		/*A2*SPRINGFOX*/
+		{
+			"id": 128,
+			"bes_tracking_id": 128,
+			"issue_url": "https://github.com/Be-Secure/BeSLighthouse/issues/128",
+			"name": "springfox",
+			"full_name": "Be-Secure/springfox",
+			"description": "Automated JSON API documentation for API's built with Spring",
+			"watchers_count": 0,
+			"forks_count": 0,
+			"stargazers_count": 0,
+			"size": 186294,
+			"open_issues": 0,
+			"created_at": "2021-08-05T09:58:34Z",
+            "updated_at": "2021-08-05T09:58:35Z",
+            "pushed_at": "2021-08-05T09:23:42Z",
+			
+			"git_url": "git://github.com/Be-Secure/springfox.git",
+			"clone_url": "https://github.com/Be-Secure/springfox.git",
+			"html_url": "https://github.com/springfox/springfox",
+			"homepage": "http://springfox.io",
+			
+			"owner": {
+				"login": "Be-Secure",
+				"id": 44028837 ,
+				"avatar_url": "https://avatars.githubusercontent.com/u/44028837?v=4",
+				"html_url": "https://github.com/Be-Secure"
+			},
+            "bes_technology_stack": "A",
+			"project_repos": {
+				"main_github_url": "https://github.com/chatwoot/springfox",
+				"main_bes_url": "https://github.com/Be-Secure/springfox",
+				"all_projects": {
+					"springfox": "https://github.com/spring/springfox"
+
+				},
+				"all_bes_repos": [{
+						"id": 3333858979 ,
+						"name": "springfox",
+						"url": "https://github.com/Be-Secure/springfox"
+					}
+
+				]
+			},
+			"license": {
+				"key": "apache-2.0",
+    			"name": "Apache License 2.0",
+    			"spdx_id": "Apache-2.0",
+    			"url": "https://api.github.com/licenses/apache-2.0",
+    			"node_id": "MDc6TGljZW5zZTI="
+				},
+			"language": [
+
+				{
+					"Java": 491049,
+  					"Groovy": 158286,
+ 					"JavaScript": 10748,
+ 					"HTML": 3157,
+ 					"CSS": 2894,
+ 					"Vim Snippet": 1990,
+ 					"Shell": 337
+					
+				}
+			],
+			"tags": [
+				"TD-U-ApD",
+				"IND-ALL",
+				"COM - C",
+			   
+			]
+
+		},
+				/*A1CHATWOOT*/
+		{
+			"id": 145,
+			"bes_tracking_id": 145,
+			"issue_url": "https://github.com/Be-Secure/BeSLighthouse/issues/145",
+			"name": "chatwoot",
+			"full_name": "Be-Secure/chatwoot",
+			"description": "Open-source customer engagement suite, an alternative to Intercom, Zendesk, Salesforce Service Cloud etc. 🔥💬.",
+			"watchers_count": 0,
+			"forks_count": 0,
+			"stargazers_count": 0,
+			"size": 47491,
+			"open_issues": 0,
+			"created_at": "2021-08-05T09:58:34Z",
+            "updated_at": "2021-08-05T09:58:35Z",
+            "pushed_at": "2021-08-05T09:23:42Z",
+			
+			"git_url": "git://github.com/Be-Secure/chatwoot.git",
+			"clone_url": "https://github.com/Be-Secure/chatwoot.git",
+			"html_url": "https://github.com/Be-Secure/chatwoot",
+			"homepage": "https://www.chatwoot.com/help-center",
+			
+			"owner": {
+				"login": "Be-Secure",
+				"id": 44028837,
+				"avatar_url": "https://avatars.githubusercontent.com/u/44028837?v=4",
+				"html_url": "https://github.com/Be-Secure"
+			},
+            "bes_technology_stack": "A",
+			"project_repos": {
+				"main_github_url": "https://github.com/chatwoot/chatwoot",
+				"main_bes_url": "https://github.com/Be-Secure/chatwoot",
+				"all_projects": {
+					"chatwoot": "https://github.com/chatwoot/chatwoot"
+
+				},
+				"all_bes_repos": [{
+						"id": 202291859,
+						"name": "chatwoot",
+						"url": "https://github.com/Be-Secure/chatwoot"
+					}
+
+				]
+			},
+			"license": {
+				"key": "mit",
+				"name": "MIT License",
+				"spdx_id": "MIT",
+				"url": "https://api.github.com/licenses/mit",
+				"node_id": "MDc6TGljZW5zZTEz"
+			  },
+			"language": [
+
+				{
+					"JavaScript": 754789,
+					"Shell": 58
+				}
+			],
+
+			"tags": [
+				"L&F",
+				"IND-ALL",
+				"COM - C",
+			   
+			]
+
+		},
+		{
+			"id": 128,
+			"bes_tracking_id": 152,
+			"issue_url": "https://github.com/Be-Secure/BeSLighthouse/issues/152",
+			"name": "joplin",
+			"full_name": "Be-Secure/joplin",
+			"description": "Joplin - an open source note taking and to-do application with synchronization capabilities for Windows, macOS, Linux, Android and iOS. Forum: https://discourse.joplinapp.org/",
+			"watchers_count": 0,
+			"forks_count": 0,
+			"stargazers_count": 0,
+			"size": 215192,
+			"open_issues": 0,
+			"created_at": "2017-01-16T21:49:41Z",
+    		"updated_at": "2023-02-23T18:59:10Z",
+    		"pushed_at": "2023-02-23T16:42:53Z",
+			"git_url": "git://github.com/Be-Secure/joplin.git",
+			"clone_url": "https://github.com/Be-Secure/joplin.git",
+			"html_url": "https://github.com/Be-Secure/joplin",
+			"homepage": "https://joplinapp.org",
+			"owner": {
+				"login": "Be-Secure",
+				"id": 44028837,
+				"avatar_url": "https://avatars.githubusercontent.com/u/44028837?v=4",
+				"html_url": "https://github.com/Be-Secure"
+			},
+            "bes_technology_stack": "A",
+			"project_repos": {
+				"main_github_url": "https://github.com/laurent22/joplin",
+				"main_bes_url": "https://github.com/Be-Secure/joplin",
+				"all_projects": {
+					"joplin": "https://github.com/laurent22/joplin"
+
+				},
+				"all_bes_repos": [{
+						"id": 79162682,
+						"name": "joplin",
+						"url": "https://github.com/Be-Secure/joplin"
+					}
+
+				]
+			},
+			"license": {
+				"key": "other",
+				"name": "Other",
+				"spdx_id": "NOASSERTION",
+				"url": null,
+				"node_id": "MDc6TGljZW5zZTA="
+			},
+			"language": [
+				{
+					"TypeScript": 3501550,
+					"JavaScript": 2959974,
+					"HTML": 130183,
+					"CSS": 101881,
+					"Mustache": 48814,
+					"Handlebars": 31635,
+					"Shell": 22781,
+					"Java": 20784,
+					"Objective-C": 16633,
+					"Ruby": 2194,
+					"Starlark": 1920,
+					"C": 419,
+					"Batchfile": 140
+				}
+			],
+
+			"tags": [
+				"A",
+				"IND-ALL",
+				"COM - C",
+				"TD-U-W",
+				"TD-U-DkA",
+				"TD-U-MoA",
+			   
+			]
+
+		}, 
+		/*A3SPRING-SECURITY*/
+		{
+			"id": 144,
+			"bes_tracking_id": 144,
+			"issue_url": "https://github.com/Be-Secure/BeSLighthouse/issues/144",
+			"name": "spring-security",
+			"full_name": "Be-Secure/spring-security",
+			"description": "Spring Security",
+			"watchers_count": 0,
+			"forks_count": 0,
+			"stargazers_count": 0,
+			"size": 71143,
+			"open_issues": 0,
+			"created_at": "2023-02-24T07:32:41Z",
+  			"updated_at": "2023-02-24T06:19:14Z",
+  			"pushed_at": "2023-02-24T02:47:32Z",
+			
+			
+			"git_url": "git://github.com/Be-Secure/spring-securityc.git",
+			"clone_url": "https://github.com/Be-Secure/spring-security.git",
+			"html_url": "https://github.com/Be-Secure/spring-security",
+			"homepage": "http://spring.io/projects/spring-security",
+			
+			"owner": {
+				"login": "Be-Secure",
+				"id": 44028837,
+				"avatar_url": "https://avatars.githubusercontent.com/u/44028837?v=4",
+				"html_url": "https://github.com/Be-Secure"
+			},
+            "bes_technology_stack": "A",
+			"project_repos": {
+				"main_github_url": "https://github.com/spring-security/spring-security",
+				"main_bes_url": "https://github.com/Be-Secure/spring-security",
+				"all_projects": {
+					"spring-security": "https://github.com/spring-security/spring-security"
+
+				},
+				"all_bes_repos": [{
+						"id": 44028837,
+						"name": "spring-security",
+						"url": "https://github.com/Be-Secure/spring-security"
+					}
+
+				]
+			},
+			"license": {
+				"key": "apache-2.0",
+				"name": "Apache License 2.0",
+				"spdx_id": "Apache-2.0",
+				"url": "https://api.github.com/licenses/apache-2.0",
+				"node_id": "MDc6TGljZW5zZTI="
+			  },
+			"language": [
+
+				{
+					"Java": 15872722,
+  					"Kotlin": 845502,
+  					"Groovy": 43860,
+  					"AspectJ": 16776,
+  					"Ruby": 7008,
+  					"PLSQL": 3180,
+  					"XSLT": 2369,
+ 					"Shell": 811,
+  					"Python": 129,
+  					"HTML": 80,
+  					"JavaScript": 10
+					
+				}
+			],
+			"tags": [
+				"TD-C-WA",
+				"IND-ALL",
+				"COM - C",
+			   
+			]
+
+		},
+		/*A4HEALTHCHECKS*/
+		{
+			"id": 143,
+			"bes_tracking_id": 143,
+			"issue_url": "https://github.com/Be-Secure/BeSLighthouse/issues/143",
+			"name": "healthchecks",
+			"full_name": "Be-Secure/healthchecks",
+			"description": "A cron monitoring tool written in Python & Django",
+			"watchers_count": 0,
+			"forks_count": 0,
+			"stargazers_count": 0,
+			"size": 20334,
+			"open_issues": 0,
+			"created_at": "2023-02-24T07:34:26Z",
+  			"updated_at": "2023-02-24T02:55:18Z",
+  			"pushed_at": "2023-02-21T09:11:50Z",  
+			
+			"git_url": "git://github.com/healthchecks/healthchecks.git",
+			"clone_url": "https://github.com/healthchecks/healthchecks.git",
+			"html_url": "https://github.com/Be-Secure/healthchecks",
+			"homepage": "https://healthchecks.io",
+
+			"git_url": "git://github.com/Be-Secure/healthchecks.git",
+			"clone_url": "https://github.com/Be-Secure/healthchecks.git",
+			"homepage": "https://www.healthchecks.com/help-center",
+			
+			"owner": {
+				"login": "Be-Secure",
+    			"id": 44028837,
+    			"avatar_url": "https://avatars.githubusercontent.com/u/44028837?v=4",
+				"login": "Be-Secure",
+				"id": 44028837,
+				"avatar_url": "https://avatars.githubusercontent.com/u/44028837?v=4",
+				"html_url": "https://github.com/Be-Secure"
+			},
+            "bes_technology_stack": "A",
+			"project_repos": {
+				"main_github_url": "https://github.com/healthchecks/healthchecks",
+				"main_bes_url": "https://github.com/Be-Secure/healthchecks",
+				"all_projects": {
+					"chatwoot": "https://github.com/healthchecks/healthcheck"
+
+				},
+				"all_bes_repos": [{
+						"id": 38067078,
+						"name": "healthcheck",
+						"url": "https://github.com/Be-Secure/healthcheck"
+					}
+
+				]
+			},
+			"license": {
+				"key": "bsd-3-clause",
+				"name": "BSD 3-Clause \"New\" or \"Revised\" License",
+				"spdx_id": "BSD-3-Clause",
+				"url": "https://api.github.com/licenses/bsd-3-clause",
+				"node_id": "MDc6TGljZW5zZTU="
+			  },
+			"language": [
+				{
+					"Python": 1089200,
+  					"HTML": 842757,
+  					"Less": 211299,
+  					"CSS": 66400,
+  					"JavaScript": 51271,
+  					"Shell": 1668,
+  					"Dockerfile": 1088
+				}
+			],
+			"tags": [
+				"TD-U-W",
+				"IND-ALL",
+				"COM-C",
+			   
+			]
+
+		},
+		/*A5 SALEOR*/
+		{
+			"id": 126,
+			"bes_tracking_id": 126,
+			"issue_url": "https://github.com/Be-Secure/BeSLighthouse/issues/126",
+			"name": "saleor",
+			"full_name": "Be-Secure/saleor",
+			"description": "A modular, high performance, headless e-commerce platform built with Python, GraphQL, Django, and React.",
+			"watchers_count": 0,
+			"forks_count": 0,
+			"stargazers_count": 0,
+			"size": 111120,
+			"open_issues": 0,
+			"created_at": "2021-08-05T09:54:20Z",
+  			"updated_at": "2021-10-06T10:36:32Z",
+  			"pushed_at": "2021-08-05T09:38:10Z",
+			"git_url": "git://github.com/healthchecks/saleor.git",
+			"clone_url": "https://github.com/healthchecks/saleor.git",
+			"html_url": "https://github.com/Be-Secure/saleor",
+			"homepage": "https://saleor.io",
+
+			"git_url": "git://github.com/Be-Secure/saleor.git",
+			"clone_url": "https://github.com/Be-Secure/saleor.git",
+			"homepage": "https://www.saleor.io",
+			
+			"owner": {
+				"login": "Be-Secure",
+    			"id": 44028837,
+    			"avatar_url": "https://avatars.githubusercontent.com/u/44028837?v=4",
+				"login": "Be-Secure",
+				"id": 44028837,
+				"avatar_url": "https://avatars.githubusercontent.com/u/44028837?v=4",
+				"html_url": "https://github.com/Be-Secure"
+			},
+            "bes_technology_stack": "A",
+			"project_repos": {
+				"main_github_url": "https://github.com/saleor/saleor",
+				"main_bes_url": "https://github.com/Be-Secure/saleor",
+				"all_projects": {
+					"saleor": "https://github.com/saleor/saleor"
+
+				},
+				"all_bes_repos": [{
+						"id": 13796165,
+						"name": "saleor",
+						"url": "https://github.com/Be-Secure/saleor"
+					}
+
+				]
+			},
+			"license": {
+				"key": "other",
+    			"name": "Other",
+    			"spdx_id": "NOASSERTION",
+    			"url": null,
+    			"node_id": "MDc6TGljZW5zZTA="
+
+			  },
+			"language": [
+
+				{
+					"Python": 7567521,
+  					"HTML": 240695,
+  					"Dockerfile": 2169,
+  					"Shell": 439
+					
+					
+				}
+			],
+			"tags": [
+				"TD-U-W",
+				"IND-RT",
+				"COM-C",
+			   
+			]
+
+		}
+
+
+		{
+			"id": 128,
+			"bes_tracking_id": 152,
+			"issue_url": "https://github.com/Be-Secure/BeSLighthouse/issues/152",
+			"name": "joplin",
+			"full_name": "Be-Secure/joplin",
+			"description": "Joplin - an open source note taking and to-do application with synchronization capabilities for Windows, macOS, Linux, Android and iOS. Forum: https://discourse.joplinapp.org/",
+			"watchers_count": 0,
+			"forks_count": 0,
+			"stargazers_count": 0,
+			"size": 215192,
+			"open_issues": 0,
+			"created_at": "2017-01-16T21:49:41Z",
+    		"updated_at": "2023-02-23T18:59:10Z",
+    		"pushed_at": "2023-02-23T16:42:53Z",
+			"git_url": "git://github.com/Be-Secure/joplin.git",
+			"clone_url": "https://github.com/Be-Secure/joplin.git",
+			"html_url": "https://github.com/Be-Secure/joplin",
+			"homepage": "https://joplinapp.org",
+			"owner": {
+				"login": "Be-Secure",
+				"id": 44028837,
+				"avatar_url": "https://avatars.githubusercontent.com/u/44028837?v=4",
+				"html_url": "https://github.com/Be-Secure"
+			},
+            "bes_technology_stack": "A",
+			"project_repos": {
+				"main_github_url": "https://github.com/laurent22/joplin",
+				"main_bes_url": "https://github.com/Be-Secure/joplin",
+				"all_projects": {
+					"joplin": "https://github.com/laurent22/joplin"
+
+				},
+				"all_bes_repos": [{
+						"id": 79162682,
+						"name": "joplin",
+						"url": "https://github.com/Be-Secure/joplin"
+					}
+
+				]
+			},
+			"license": {
+				"key": "other",
+				"name": "Other",
+				"spdx_id": "NOASSERTION",
+				"url": null,
+				"node_id": "MDc6TGljZW5zZTA="
+			},
+			"language": [
+				{
+					"TypeScript": 3501550,
+					"JavaScript": 2959974,
+					"HTML": 130183,
+					"CSS": 101881,
+					"Mustache": 48814,
+					"Handlebars": 31635,
+					"Shell": 22781,
+					"Java": 20784,
+					"Objective-C": 16633,
+					"Ruby": 2194,
+					"Starlark": 1920,
+					"C": 419,
+					"Batchfile": 140
+				}
+			],
+
+			"tags": [
+				"A",
+				"IND-ALL",
+				"COM - C",
+				"TD-U-W",
+				"TD-U-DkA", 
+				"TD-U-MoA",
+			   
+			]
+
+		},
+		
+		{
 			"id": 152,
 			"bes_tracking_id": 152,
 			"issue_url": "https://github.com/Be-Secure/BeSLighthouse/issues/152",
@@ -1541,6 +2145,12 @@ const projectOfInterestObject = {
 				"TD-U-MoA",
 			   
 			]
+
+		}
+          
+
+		
+
 
 		}
 		
