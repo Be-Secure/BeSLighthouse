b5e30dcd00456ac02aa4c4e660044ca03501017f
Arun Suresh <58501088+asa1997@users.noreply.github.com>
2023-03-21 11:43:41 +0530

    Added visualisation for license compliance (#435)
    * Fixed #328
    
    * Removed comments
    
    * Sanity check for report availability added

--- bes_theme/assets/javascripts/assessment_reports.js
+++ bes_theme/assets/javascripts/assessment_reports.js
@@ -117,6 +117,22 @@ function print_sonarqube_report(data) {
   document.getElementById("reports").innerHTML = table;
 }
 
+function print_fossology_report(data) 
+{
+  json_data = JSON.stringify(data);
+  // console.log(json_data);
+  json_object = JSON.parse(json_data);
+  var table = "<table id=table>"
+  table += "<tr><th>FileName</th><th>License Concluded</th><th>File Copyright Text</th></tr>"
+  for (let i in json_object) {
+    table += "<tr><td>" + json_object[i].FileName + "</td><td>" + json_object[i].LicenseConcluded + "</td><td>" + json_object[i].FileCopyrightText + "</td></tr>"
+
+  }
+      
+  table += "</table>"
+  document.getElementById("reports").innerHTML = table;
+}
+
 function fetch_json()
 {
   var url;
@@ -127,8 +143,13 @@ function fetch_json()
   console.log("report:"+report);
   if (report == "codeql" || report == "sonarqube") { // The sast reports(codeql, sonarqube, ...) are under sast dir.
     url = 'https://raw.githubusercontent.com/Be-Secure/besecure-assessment-datastore/main/'+ ossp_name + '/' + version + '/sast' + '/' + ossp_name+ '-' + version + '-' + report + '-report.json';  
-  } 
-    else {
+  }
+
+  else if (report == "fossology")
+  {
+    url = 'https://raw.githubusercontent.com/Be-Secure/besecure-assessment-datastore/main/'+ ossp_name + '/' + version + '/license-compliance' + '/' + ossp_name+ '-' + version + '-' + report + '-report.json';
+  }
+  else {
     url = 'https://raw.githubusercontent.com/Be-Secure/besecure-assessment-datastore/main/'+ ossp_name + '/' + version + '/' + report + '/' + ossp_name+ '-' + version + '-' + report + '-report.json';
 
   }
@@ -161,6 +182,10 @@ function fetch_json()
     } else if (report == "sonarqube"){
       
       print_sonarqube_report(data);
+    
+    } else if (report == "fossology"){
+      
+      print_fossology_report(data);
     }
 
 
--- bes_theme/assets/javascripts/version_summary.js
+++ bes_theme/assets/javascripts/version_summary.js
@@ -110,7 +110,13 @@ function check_url(id, version, name)
   {
 
     var url = "https://raw.githubusercontent.com/Be-Secure/besecure-assessment-datastore/main/"+name+"/"+version+"/sast/"+name+"-"+version+"-"+id+"-report.json"
-  }else
+  } else if (id == "fossology")
+  {
+
+    var url = "https://raw.githubusercontent.com/Be-Secure/besecure-assessment-datastore/main/"+name+"/"+version+"/license-compliance/"+name+"-"+version+"-"+id+"-report.json"
+
+  }
+  else
   {
     var url = "https://raw.githubusercontent.com/Be-Secure/besecure-assessment-datastore/main/"+name+"/"+version+"/"+id+"/"+name+"-"+version+"-"+id+"-report.json"
 
@@ -229,6 +235,7 @@ function load_version_data(base_url) {
         <th>Criticality Score</th>
         <th>Sonarqube</th>
         <th>Codeql</th>
+        <th>Fossology</th>
       </tr>
       <tr>
         <td>${releaseData}</td>
@@ -237,6 +244,7 @@ function load_version_data(base_url) {
         ${criticalityScore_table_td_data}        
         <td><a id="sonarqube"; href='javascript:open_report("${base_url}","${version}", "sonarqube", "${ossp_name}")'>Click here</a></td>
         <td><a id="codeql" href='javascript:open_report("${base_url}","${version}", "codeql", "${ossp_name}")'>Click here</a></td>
+        <td><a id="fossology" href='javascript:open_report("${base_url}","${version}", "fossology", "${ossp_name}")'>Click here</a></td>
       </tr>
       `;
       version_table.innerHTML = html_for_table;
@@ -268,6 +276,7 @@ function load_version_data(base_url) {
       check_url("scorecard", version, ossp_name);
       check_url("sonarqube", version, ossp_name);
       check_url("codeql", version, ossp_name);
+      check_url("fossology", version, ossp_name);
     }
 
 
