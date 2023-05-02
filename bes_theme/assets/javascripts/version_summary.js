0a085d6090b630aaabcfc7bc0746afd07b15ac46
sand-hya <sandhya.k91@wipro.com>
2023-05-02 13:41:21 +0530

    version drop down

--- bes_theme/assets/javascripts/version_summary.js
+++ bes_theme/assets/javascripts/version_summary.js
@@ -7,41 +7,41 @@ function vulnsbytypeandyearchart(chart_Id, cve_details) {
     cve_details.innerHTML = `<h3 class="cve-not-available-css">CVE details are not available at the moment</h3>`
     return;
   }
-  let detail_cve = {} 
-  for (let i=0; i<cve_details.length; i++) {
-    const key =  Object.keys(cve_details[i]);
-    for (let j=0; j<key.length; j++) {
-        if (key[j] !== 'Year') {
-          if (!detail_cve[key[j]]) {
-            detail_cve[key[j]] = [];
-          }
-          if (cve_details[i][key[j]] !== null  && cve_details[i][key[j]] !== "") {
-            detail_cve[key[j]].push([parseInt(cve_details[i]["Year"]), cve_details[i][key[j]]]);
-          }
+  let detail_cve = {}
+  for (let i = 0; i < cve_details.length; i++) {
+    const key = Object.keys(cve_details[i]);
+    for (let j = 0; j < key.length; j++) {
+      if (key[j] !== 'Year') {
+        if (!detail_cve[key[j]]) {
+          detail_cve[key[j]] = [];
         }
+        if (cve_details[i][key[j]] !== null && cve_details[i][key[j]] !== "") {
+          detail_cve[key[j]].push([parseInt(cve_details[i]["Year"]), cve_details[i][key[j]]]);
+        }
+      }
     }
   }
-  const data=[];
-  const vulntypesarray=[];
+  const data = [];
+  const vulntypesarray = [];
   const visible_all_field = []
   const key = Object.keys(detail_cve);
-  for (let i=0; i<key.length; i++) {
+  for (let i = 0; i < key.length; i++) {
     if (detail_cve[key[i]].length === 0) {
-      visible_all_field.push({show: false, label: key[i]});
+      visible_all_field.push({ show: false, label: key[i] });
     } else {
-      visible_all_field.push({show: true, label: key[i]});
+      visible_all_field.push({ show: true, label: key[i] });
     }
     vulntypesarray.push(key[i]);
     data.push(detail_cve[key[i]]);
   }
-  $.jqplot (chart_Id, data, {
+  $.jqplot(chart_Id, data, {
     seriesColors: [
-      '#69c','#333','#DB045B','#E04807',
-      '#4572A7', '#AA4643', '#89A54E', '#80699B', '#3D96AE','#DB843D', '#92A8CD', '#A47D7C', '#B5CA92','#FF9655','#24CBE5'
+      '#69c', '#333', '#DB045B', '#E04807',
+      '#4572A7', '#AA4643', '#89A54E', '#80699B', '#3D96AE', '#DB843D', '#92A8CD', '#A47D7C', '#B5CA92', '#FF9655', '#24CBE5'
     ],
     seriesDefaults: {},
-    series:visible_all_field,
-    title:{
+    series: visible_all_field,
+    title: {
       text: 'Vulnerabilities by type & year',
       fontFamily: '"Lucida Grande", "Lucida Sans Unicode", Verdana, Arial, Helvetica, sans-serif', // default font
       fontSize: '12px',
@@ -56,117 +56,126 @@ function vulnsbytypeandyearchart(chart_Id, cve_details) {
         label: "Years",
         color: '#3E576F',
         pad: 0,
-        min:1999,
+        min: 1999,
         tickInterval: 1
       },
       yaxis: {
         label: "# Of Vulns",
-        numberTicks : 5,
-        min:0 ,
+        numberTicks: 5,
+        min: 0,
         color: '#3E576F'
       }
     },
     legend: {
-      show:true,
+      show: true,
       location: 's',
       renderer: $.jqplot.EnhancedLegendRenderer,
       placement: 'outsideGrid',
       labels: vulntypesarray,
-      rendererOptions:{
+      rendererOptions: {
         seriesToggle: true,
-        seriesToggleReplot: {resetAxes:['yaxis']},
+        seriesToggleReplot: { resetAxes: ['yaxis'] },
         numberRows: 5,
         numberColumns: 5
       },
-      border:'1px solid #909090',
+      border: '1px solid #909090',
       fontFamily: '"Lucida Grande", "Lucida Sans Unicode", Verdana, Arial, Helvetica, sans-serif', // default font
       fontSize: '11px'
     },
     highlighter: {
       show: true,
       sizeAdjust: 12,
-      useAxesFormatters:false,
+      useAxesFormatters: false,
       fadeTooltip: false,
       formatString: "%d : %s",
-      bringSeriesToFront:true,
-      lineWidthAdjust:10
+      bringSeriesToFront: true,
+      lineWidthAdjust: 10
     }
   });
 }
 
 
-function open_report(base_url,version, report, project_name) {
+function open_report(base_url, version, report, project_name) {
   localStorage["version"] = version;
   localStorage["report"] = report;
   localStorage["ossp_name"] = project_name;
-  window.open(base_url+"/bes_assessment_reports", "_self");
+  window.open(base_url + "/bes_assessment_reports", "_self");
 }
 
-function check_url(id, version, name)
-{
-   
-  if (id == "sonarqube" || id == "codeql")
-  {
+function check_url(id, version, name) {
 
-    var url = "https://raw.githubusercontent.com/Be-Secure/besecure-assessment-datastore/main/"+name+"/"+version+"/sast/"+name+"-"+version+"-"+id+"-report.json"
+  if (id == "sonarqube" || id == "codeql") {
+
+    var url = "https://raw.githubusercontent.com/Be-Secure/besecure-assessment-datastore/main/" + name + "/" + version + "/sast/" + name + "-" + version + "-" + id + "-report.json"
 
   }
 
-  else if (id == "fossology")
-  {
+  else if (id == "fossology") {
 
-    var url = "https://raw.githubusercontent.com/Be-Secure/besecure-assessment-datastore/main/"+name+"/"+version+"/license-compliance/"+name+"-"+version+"-"+id+"-report.json"
+    var url = "https://raw.githubusercontent.com/Be-Secure/besecure-assessment-datastore/main/" + name + "/" + version + "/license-compliance/" + name + "-" + version + "-" + id + "-report.json"
 
   }
-  
-  else
-  {
-    var url = "https://raw.githubusercontent.com/Be-Secure/besecure-assessment-datastore/main/"+name+"/"+version+"/"+id+"/"+name+"-"+version+"-"+id+"-report.json"
+
+  else {
+    var url = "https://raw.githubusercontent.com/Be-Secure/besecure-assessment-datastore/main/" + name + "/" + version + "/" + id + "/" + name + "-" + version + "-" + id + "-report.json"
 
   }
 
   fetch(url, {
     method: 'GET',
     headers: {
-        'Accept': 'application/json',
+      'Accept': 'application/json',
     },
-})
-  .then(function (response) {
-    return response.json();
   })
-  .then(function (data) {
-    
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
 
 
-  })
-  .catch(function (err) {
-    document.getElementById(id).setAttribute('href','javascript:void(0)');
-    document.getElementById(id).innerHTML = "Not Available"
-    document.getElementById(id).classList.add("disabled-link")
-  });
+
+    })
+    .catch(function (err) {
+      document.getElementById(id).setAttribute('href', 'javascript:void(0)');
+      document.getElementById(id).innerHTML = "Not Available"
+      document.getElementById(id).classList.add("disabled-link")
+    });
 }
 
 function load_version_data(base_url) {
   id = localStorage["id"]
   ossp_name = localStorage["name"];
 
-  fetch('https://raw.githubusercontent.com/Be-Secure/besecure-osspoi-datastore/main/version_details/'+id+'-'+ossp_name+'-Versiondetails.json')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    const container_element = document.getElementById("container");
-    const create_hadder_content = document.getElementById("hadder_page");
-    const span_for_hadder = document.getElementById("hadder-css-page");
-    span_for_hadder.innerText = "BeSLighthouse"
-    create_hadder_content.append(span_for_hadder);
-    const report_name = document.getElementById("report-name-project");
-    const span_name = `<span class="span-report-css">Project: ${ossp_name}</span>`;
-    report_name.innerHTML = span_name;
+  fetch('https://raw.githubusercontent.com/sand-hya/besecure-osspoi-datastore/besissue/version_details/' + id + '-' + ossp_name + '-Versiondetails.json')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      array = [];
+      for (let i = 0; i < Object.keys(data).length; i++) {
+        array.push(data[i].version);
+      }
+      //adding versions to the dropdown list
+      var temp = document.getElementById("selectversion");
+      for (var i = 0; i < array.length; i++) {
+        var opt = array[i];
+        var el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        temp.appendChild(el);
+      }
 
-    // For each version of the project, we are creating different tags on the fly.
-    for (let i = 0; i<Object.keys(data).length; i++) {
-      
+      const container_element = document.getElementById("container");
+      const create_hadder_content = document.getElementById("hadder_page");
+      const span_for_hadder = document.getElementById("hadder-css-page");
+      span_for_hadder.innerText = "BeSLighthouse"
+      create_hadder_content.append(span_for_hadder);
+      const report_name = document.getElementById("report-name-project");
+      const span_name = `<span class="span-report-css">Project: ${ossp_name}</span>`;
+      report_name.innerHTML = span_name;
+
+      // For each version of the project, we are creating different tags on the fly.
+
       // Main div
       const main_div_content = document.getElementById("border-div-css");
       // version table
@@ -178,60 +187,56 @@ function load_version_data(base_url) {
       const version_table1 = document.getElementById("version_table1");
       const try_div = document.getElementById("border_merge_css");
 
-      i=0;
+      i = 0;
       const releaseData = data[i].release_date;
       const version = data[i].version;
       const scorecard = data[i].scorecard;
-      const criticalityScore = data[i].criticality_score;  
+      const criticalityScore = data[i].criticality_score;
 
       const scorecardLink = `<a id="scorecard" href='javascript:open_report("${base_url}","${version}", "scorecard", "${ossp_name}")'>${scorecard}</a>`;
 
-      if (scorecard != "Not Available")
-      {
+      if (scorecard != "Not Available") {
         // Color coding scorecard scores
-        if (scorecard <= 2.5 ) {
-      
+        if (scorecard <= 2.5) {
+
           scorecard_table_td_data = `<span style="background-color:green"; >${scorecardLink}</span>`
-    
-        } else if (scorecard <= 5 ){
-         
+
+        } else if (scorecard <= 5) {
+
           scorecard_table_td_data = `<span style="background-color:yellow"; >${scorecardLink}</span>`
 
-        } else if (scorecard <= 7.5 ){
-         
+        } else if (scorecard <= 7.5) {
+
           scorecard_table_td_data = `<span style="background-color:orange"; >${scorecardLink}</span>`
 
-        } else if (scorecard <= 10 ){
-       
+        } else if (scorecard <= 10) {
+
           scorecard_table_td_data = `<span style="background-color:red"; >${scorecardLink}</span>`
-        } 
-      } else
-      {
-       
-        scorecard_table_td_data=`<span >${scorecardLink}</span>`
+        }
+      } else {
+
+        scorecard_table_td_data = `<span >${scorecardLink}</span>`
 
       }
-      
-      if (criticalityScore != "Not Available")
-      {
-        if (criticalityScore < 0.5 ) {
-         
+
+      if (criticalityScore != "Not Available") {
+        if (criticalityScore < 0.5) {
+
           criticalityScore_table_td_data = `<span style="background-color:green; color:white" >${criticalityScore}</span>`
-          
-        } else if (criticalityScore == 0.5 ){
-          
+
+        } else if (criticalityScore == 0.5) {
+
           criticalityScore_table_td_data = `<span style="background-color:yellow" >${criticalityScore}</span>`
-          
-        } else if (criticalityScore > 0.5 ){
- 
+
+        } else if (criticalityScore > 0.5) {
+
           criticalityScore_table_td_data = `<span style="background-color:red; color:white" >${criticalityScore}</span>`
-        
+
         }
-      } else
-      {
-        criticalityScore_table_td_data=`<span >${criticalityScore}</span>`
-      }     
-      
+      } else {
+        criticalityScore_table_td_data = `<span >${criticalityScore}</span>`
+      }
+
       // Graph code
       const chart_Id = `bar_chart_vuln_by_type${i}`;
       const div_tag_chat_main = document.createElement("div");
@@ -241,17 +246,17 @@ function load_version_data(base_url) {
       div_tag_for_chat.setAttribute("id", chart_Id);
       div_tag_for_chat.style.height = "400px";
       div_tag_chat_main.append(div_tag_for_chat);
-    
+
       // Append in div
       main_div_content.appendChild(div_tag_chat_main);
-      
+
       // All the above created elements are added into div tag with id version_details. 
       const bottom_div = document.createElement("div");
       bottom_div.className = "bottom-div";
       container_element.appendChild(bottom_div);
 
 
-      $(document).ready(function() {
+      $(document).ready(function () {
         vulnsbytypeandyearchart(chart_Id, data[i].cve_details);
       });
       check_url("scorecard", version, ossp_name);
@@ -259,15 +264,14 @@ function load_version_data(base_url) {
       check_url("codeql", version, ossp_name);
       check_url("sbom", version, ossp_name);
       check_url("fossology", version, ossp_name);
-    }
 
 
-  })
-  
-  .catch(function (err) {
-  
-    console.dir(err);
-  
-  });
+    })
+
+    .catch(function (err) {
+
+      console.dir(err);
+
+    });
 
 }
