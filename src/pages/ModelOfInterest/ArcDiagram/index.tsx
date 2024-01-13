8516dd0e745c522503c9a9d2d598fc67cd7d1ff3
pramit <pramit@vbox.myguest.virtualbox.org>
2024-01-14 00:25:16 +0530

    Added click functionality

--- src/pages/ModelOfInterest/ArcDiagram/index.tsx
+++ src/pages/ModelOfInterest/ArcDiagram/index.tsx
@@ -15,16 +15,16 @@ const ArcDiagram: React.FC = () => {
       })
       .then(data => {
         // set the dimensions and margins of the graph
-        const margin = { top: 0, right: 30, bottom: 50, left: 60 },
-          width = 650 - margin.left - margin.right,
-          height = 400 - margin.top - margin.bottom;
+        const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+        const width = window.innerWidth - margin.left - margin.right;
+        const height = window.innerHeight - margin.top - margin.bottom;
 
         // append the svg object to the body of the page
         const svg = d3.select("#my_dataviz")
           .append("svg")
-          .attr("viewBox", [0, 0, width + 90, height + 80].join(' '))
-          .attr("width", width + margin.left + margin.right)
-          .attr("height", height + margin.top + margin.bottom)
+          .attr("viewBox", [0, 0, width, height + 150])
+          .attr("width", width)
+          .attr("height", height)
           .append("g")
           .attr("transform", `translate(${margin.left},${margin.top})`);
 
@@ -32,35 +32,47 @@ const ArcDiagram: React.FC = () => {
         const jsonData = data;
 
         // Extract unique nodes and links from the JSON data
-        const nodes: { name: string }[] = [];
-        const links: { source: string; target: string }[] = [];
+        const nodes: { name: string, group: string }[] = [];
+        const links: { source: string, target: string }[] = [];
 
         jsonData.forEach(node => {
-          nodes.push({ name: node.name });
+          const mainNode = { name: node.name, group: node.group };
+          nodes.push(mainNode);
           node.dependencies.forEach(dependency => {
-            nodes.push({ name: dependency });
-            links.push({ source: node.name, target: dependency });
+            const dependentNode = { name: dependency, group: node.group };
+            nodes.push(dependentNode);
+            links.push({ source: mainNode.name, target: dependentNode.name });
           });
         });
 
-        // A color scale for nodes
-        const color = d3.scaleOrdinal()
+        // A color scale for nodes based on unique names
+        const color = d3.scaleOrdinal<string>()
           .domain(nodes.map(d => d.name))
-          .range(d3.schemeSet3);
+          .range(d3.schemeCategory10);
 
         // A linear scale to position the nodes on the X axis
         const x = d3.scalePoint()
           .range([0, width])
           .domain(nodes.map(d => d.name));
 
-       // Add the links
-      const linkSelection = svg
+        // Add some space between main node and its dependencies
+        const spaceBetween = 50;
+        const spacedNodes = nodes.map((node, index) => {
+          const xPos = x(node.name);
+          return {
+            ...node,
+            x: xPos !== undefined ? xPos + (node.group === 'main' ? spaceBetween : 0) : undefined
+          };
+        }).filter(node => node.x !== undefined);
+
+        // Add the links
+        const linkSelection = svg
         .selectAll('mylinks')
         .data(links)
         .join('path')
         .attr('d', d => {
-          const start = x(d.source);
-          const end = x(d.target);
+          const start = spacedNodes.find(node => node.name === d.source)?.x;
+          const end = spacedNodes.find(node => node.name === d.target)?.x;
 
           // Check if start and end are not undefined before using them
           if (start !== undefined && end !== undefined) {
@@ -70,59 +82,119 @@ const ArcDiagram: React.FC = () => {
           }
         })
         .style("fill", "none")
-        .attr("stroke", "grey")
+        .attr("stroke", d => color(d.source) as string)
         .style("stroke-width", 1);
 
+        // Add the circle for the nodes
+        const nodeSelection = svg
+          .selectAll("mynodes")
+          .data(spacedNodes)
+          .join("circle")
+          .attr("cx", (d: { name: string, x: number | undefined }) => d.x !== undefined ? d.x : 0)  // Explicitly provide type information
+          .attr("cy", height - 30)
+          .attr("r", 7)
+          .style("fill", d => color(d.name) as string)
+          .attr("stroke", "white")
+          .on('click', (event, d) => {
+            if (isClickable(d.name) && !event.active) {
+              window.open(`/BeSLighthouse/model_report/:${d.name}`, "_blank");
+            }
+          });
 
-       // Add the circle for the nodes
-      const nodeSelection = svg
-        .selectAll("mynodes")
-        .data(nodes)
-        .join("circle")
-        .attr("cx", (d: { name: string }) => x(d.name) || 0)  // Explicitly provide type information
-        .attr("cy", height - 30)
-        .attr("r", 7)
-        .style("fill", (d: { name: string }) => color(d.name) as string)  // Explicitly provide type information
-        .attr("stroke", "white");
-
-
+        const isClickable = (nodeName) => {
+          const res: boolean = data.find(item => item.name === nodeName);
+          return res;
+        };
+            
         // And give them a label
         const labelSelection = svg
-          .selectAll("mylabels")
-          .data(nodes)
-          .join("text")
-          .attr("x", 0)
-          .attr("y", 0)
-          .text(d => d.name)
-          .style("text-anchor", "end")
-          .attr("transform", d => `translate(${x(d.name)},${height - 15}) rotate(-45)`)
-          .style("font-size", 12);
+        .selectAll("mylabels")
+        .data(spacedNodes)
+        .join("text")
+        .attr("x", 0)
+        .attr("y", 0)
+        .text(d => d.name)
+        .style("text-anchor", "end")
+        .attr("transform", d => `translate(${d.x},${height - 15}) rotate(-45)`)
+        .style("font-size", 12)
+        .on('mouseover', function (event, d) {
+          // Highlight the nodes: every node is green except for him
+          nodeSelection.style('opacity', node => node.group === d.group ? 0.2 : 0.1);
+          d3.select(this).style('opacity', 1);
+          d3.select(this).attr("cursor", isClickable(d.name) ? "pointer" : "default");
 
+          // Highlight the directly connected nodes
+          const directlyConnectedNodes = links
+            .filter(link => link.source === d.name || link.target === d.name)
+            .map(link => link.source === d.name ? link.target : link.source);
+
+          nodeSelection
+            .filter(node => directlyConnectedNodes.includes(node.name))
+            .style('opacity', 1);
+
+          // Highlight the connections
+          linkSelection
+            .style('stroke', a => a.source === d.name || a.target === d.name ? color(d.name) as string : 'grey')
+            .style('stroke-opacity', a => a.source === d.name || a.target === d.name ? 1 : 0.2)
+            .style('stroke-width', a => a.source === d.name || a.target === d.name ? 2 : 1);
+          labelSelection
+            .style("font-size", b => b.name === d.name || directlyConnectedNodes.includes(b.name) ? 18.9 : 2)
+            .attr("y", b => b.name === d.name || directlyConnectedNodes.includes(b.name) ? 10 : 0);
+          nodeSelection
+            .filter(node => node.name === d.name)
+            .style('opacity', 1)
+            .attr("cursor", isClickable(d.name) ? "pointer" : "default");
+        }).on('mouseout', function () {
+          // Reset the styles on mouseout
+          nodeSelection.style('opacity', 1);
+          d3.select(this).attr("cursor", "default");
+          linkSelection
+            .style('stroke', d => color(spacedNodes.find(node => node.name === d.source)?.name || "") as string)
+            .style('stroke-opacity', 1)
+            .style('stroke-width', 1);
+          labelSelection.style("font-size", 12);
+          nodeSelection.style('opacity', 1);
+          d3.select(this).attr("cursor", "default");
+        })
+        .on('click', (event, d) => {
+          if (isClickable(d.name) && !event.active) {
+            window.open(`/BeSLighthouse/model_report/:${d.name}`, "_blank");
+          }
+        });
         // Add the highlighting functionality
-        // Add the highlighting functionality
-nodeSelection.on('mouseover', function (event, d) {
-  // Highlight the nodes: every node is green except of him
-  nodeSelection.style('opacity', .2);
-  d3.select(this).style('opacity', 1);
+        nodeSelection.on('mouseover', function (event, d) {
+          // Highlight the nodes: every node is green except for him
+          nodeSelection.style('opacity', node => node.group === d.group ? 0.2 : 0.1);
+          d3.select(this).style('opacity', 1);
+          d3.select(this).attr("cursor", isClickable(d.name) ? "pointer" : "default");
 
-  // Highlight the connections
-  linkSelection
-    .style('stroke', a => a.source === d.name || a.target === d.name ? color(d.name) as string : 'grey')
-    .style('stroke-opacity', a => a.source === d.name || a.target === d.name ? 1 : 0.2)
-    .style('stroke-width', a => a.source === d.name || a.target === d.name ? 4 : 1);
-  labelSelection
-    .style("font-size", b => b.name === d.name ? 18.9 : 2)
-    .attr("y", b => b.name === d.name ? 10 : 0);
-}).on('mouseout', function () {
-  // Reset the styles on mouseout
-  nodeSelection.style('opacity', 0.9);
-  linkSelection
-    .style('stroke', 'grey')
-    .style('stroke-opacity', 1)
-    .style('stroke-width', 1);
-  labelSelection.style("font-size", 12);
-});
+          // Highlight the directly connected nodes
+          const directlyConnectedNodes = links
+            .filter(link => link.source === d.name || link.target === d.name)
+            .map(link => link.source === d.name ? link.target : link.source);
 
+          nodeSelection
+            .filter(node => directlyConnectedNodes.includes(node.name))
+            .style('opacity', 1);
+
+          // Highlight the connections
+          linkSelection
+            .style('stroke', a => a.source === d.name || a.target === d.name ? color(d.name) as string : 'grey')
+            .style('stroke-opacity', a => a.source === d.name || a.target === d.name ? 1 : 0.2)
+            .style('stroke-width', a => a.source === d.name || a.target === d.name ? 2 : 1);
+          labelSelection
+            .style("font-size", b => b.name === d.name || directlyConnectedNodes.includes(b.name) ? 18.9 : 2)
+            .attr("y", b => b.name === d.name || directlyConnectedNodes.includes(b.name) ? 10 : 0);
+        }).on('mouseout', function () {
+          // Reset the styles on mouseout
+          nodeSelection.style('opacity', 1);
+          d3.select(this).attr("cursor", "default");
+          linkSelection
+            .style('stroke', d => color(spacedNodes.find(node => node.name === d.source)?.name || "") as string)
+            .style('stroke-opacity', 1)
+            .style('stroke-width', 1);
+          labelSelection.style("font-size", 12);
+        });
       })
       .catch(error => {
         console.error('Error fetching data:', error.message);
