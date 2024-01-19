5c0fca3b86077b552cdec469ada7e5a52d14ce6d
Pramit Kumar Dutta <pramit.dutta@wipro.com>
2024-01-19 10:38:51 +0530

    Added sanity check on link to BeS environment scripts in POI project card  (#554)
    * Added sanity check on link to BeS environment scripts from project card
    
    * Added updated code
    
    * update

--- src/utils/fatch_json_report.ts
+++ src/utils/fatch_json_report.ts
@@ -1,4 +1,7 @@
+import { debug } from "console";
 import https from "https";
+import { reject } from "lodash";
+import { resolve } from "path";
 
 export function fetchJsonReport(url: string): Promise<any> {
   return new Promise((resolve, reject) => {
@@ -23,6 +26,38 @@ export function fetchJsonReport(url: string): Promise<any> {
       })
       .on("error", (error: any) => {
         reject(error);
+      });
+  });
+}
+
+export function getEnvPathStatus(besName: string): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    
+    const name = besName.split('-');
+    const camelCaseString = name.map((part, index) => {
+      return index === 0 ? part : part.charAt(0).toUpperCase() + part.slice(1);
+    }).join('');
+
+    const bturl = `https://raw.githubusercontent.com/Be-Secure/besecure-ce-env-repo/master/${camelCaseString}/0.0.1/besman-${camelCaseString}-BT-env.sh`;
+    const rturl = `https://raw.githubusercontent.com/Be-Secure/besecure-ce-env-repo/master/${camelCaseString}/0.0.1/besman-${camelCaseString}-RT-env.sh`;
+    https
+      .get(bturl, (response: any) => {
+        let code = response.statusCode;
+        if (code === 200){
+          resolve(true);
+        }
+      })
+      .on("error", (error: any) => {
+        https
+          .get(rturl, (response: any) => {
+            let code = response.statusCode;
+            if (code === 200){
+              resolve(true);
+            }
+          })
+          .on("error",(error:any) => {
+            reject(false);
+          });
       });
   });
 }
