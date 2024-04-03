cca98b1f3b0f01af0b2cde0b876e09d2c95b0dea
Sudhir Verma <sudhirvvermaa@gmail.com>
2024-04-03 21:23:09 +0530

    add apiDetailsConfig.json

--- src/dataStore.ts
+++ src/dataStore.ts
@@ -1,15 +1,15 @@
-const domainName = process.env.DOMAIN_NAME ?? 'https://raw.githubusercontent.com';
-const besNameSpace = process.env.BES_NAMESPACE ?? 'Be-Secure';
-const besBranch = process.env.BES_BRANCH ?? 'main';
+import jsonData from './apiDetailsConfig.json';
+
+const toolName = jsonData.activeTool === 'github' ? 'github' : 'gitlab';
 
 export const osspoiMaster =
-  `${domainName}/${besNameSpace}/besecure-assets-store/${besBranch}/projects/project-metadata.json`;
+  `${jsonData[toolName].apiUrl}/${jsonData[toolName].namespace}/besecure-assets-store/${jsonData[toolName].branch}/projects/project-metadata.json`;
 export const versionDetailsURL: string =
-  `${domainName}/${besNameSpace}/besecure-assets-store/${besBranch}/projects/project-version/`;
+  `${jsonData[toolName].apiUrl}/${jsonData[toolName].namespace}/besecure-assets-store/${jsonData[toolName].branch}/projects/project-version/`;
 export const assessmentDatastoreURL =
-  `${domainName}/${besNameSpace}/besecure-assessment-datastore/${besBranch}`;
+  `${jsonData[toolName].apiUrl}/${jsonData[toolName].namespace}/besecure-assessment-datastore/${jsonData[toolName].branch}`;
 export const vulnerabilityOffInterest =
-  `${domainName}/${besNameSpace}/besecure-assets-store/${besBranch}/vulnerabilities/vulnerability-metadata.json`;
+  `${jsonData[toolName].apiUrl}/${jsonData[toolName].namespace}/besecure-assets-store/${jsonData[toolName].branch}/vulnerabilities/vulnerability-metadata.json`;
 export const modelOfInterestData =
-  `${domainName}/${besNameSpace}/besecure-assets-store/${besBranch}/models/model-metadata.json`;
-export const besecureMlAssessmentDataStore = `${domainName}/${besNameSpace}/besecure-ml-assessment-datastore/${besBranch}/models`;
+  `${jsonData[toolName].apiUrl}/${jsonData[toolName].namespace}/besecure-assets-store/${jsonData[toolName].branch}/models/model-metadata.json`;
+export const besecureMlAssessmentDataStore = `${jsonData[toolName].apiUrl}/${jsonData[toolName].namespace}/besecure-ml-assessment-datastore/${jsonData[toolName].branch}/models`;
