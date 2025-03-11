99e9522bf3bd70adec1b9cbf1827134485c37b9f
asa1997 <arun.suresh@wipro.com>
2025-03-11 14:18:29 +0530

    updated

---
+++ src/pages/ShowModelDetails/AutocompleteModalDetails.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Box, Card, CardContent, Grid, Typography } from '@mui/material';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer
+} from 'recharts';
+
+export const AutocompleteModal = () => {
+
+};
