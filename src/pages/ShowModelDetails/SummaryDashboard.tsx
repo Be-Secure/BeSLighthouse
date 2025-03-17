bfda3a330c848427ddefa34a33fe3779d239743a
Arun Suresh <58501088+asa1997@users.noreply.github.com>
2025-03-17 12:45:59 +0530

    Syncing changes with main for release 0.25.0 (#678)
    * Added UI for Prompt Injection (#673)
    
    * Fixed #675; #674: Summary card for autocomplete and instruct benchmark test (#677)
    
    * Fixed #674; 675
    
    * Lable update
    
    * version pumped
    
    ---------
    
    Co-authored-by: Sudhir Verma <sudhirvvermaa@gmail.com>

--- src/pages/ShowModelDetails/SummaryDashboard.tsx
+++ src/pages/ShowModelDetails/SummaryDashboard.tsx
@@ -10,6 +10,8 @@ import { verifyLink } from "../../utils/verifyLink";
 import { besecureMlAssessmentDataStore } from "../../dataStore";
 import MitreModal from "./MitreModal";
 import { SpearPhishingModal } from "./SpearPhishingModalDetails";
+import PromptInjectionModal from "./PromptInjectionModal";
+import { AutocompleteModal } from "./AutocompleteModalDetails";
 
 const modalStyle = {
   position: 'absolute',
@@ -115,6 +117,43 @@ interface PromptInjectionStats {
 
 export type MitreDataArray = MitreData[];
 
+interface AutoCompleteDetailData {
+  prompt_id?: number;
+  pass_id?: number;
+  test_case_prompt?: string;
+  cwe_identifier?: string;
+  language?: string;
+  line_text?: string;
+  origin_code?: string;
+  variant?: string;
+  rule?: string;
+  repo?: string;
+  model?: string;
+  icd_result?: string;
+  icd_cwe_detections?: [ string ];
+  bleu_score?: number;
+  original_code?: string;
+}
+
+export type AutocompleteDetailDataArray = AutoCompleteDetailData[];
+
+
+interface PromptInjectionData {
+  prompt_id: number;
+  pass_id: number;
+  test_case_prompt: string;
+  user_input: string;
+  response: string;
+  judge_response: string;
+  injection_type: string;
+  injection_variant: string;
+  risk_category: string;
+  speaking_language: string;
+  model: string;
+}
+
+export type PromptInjectionDataArray = PromptInjectionData[];
+
 interface LanguageStats {
   bleu: number;
   total_count: number;
@@ -123,7 +162,7 @@ interface LanguageStats {
   pass_rate: number;
 }
 
-interface AutocompleteData {
+export interface AutocompleteData {
   [language: string]: LanguageStats;
 }
 
@@ -214,20 +253,34 @@ const SummaryDashboard = ({ model }: any) => {
     `${besecureMlAssessmentDataStore}/${selectedModel.name}/llm-benchmark/${selectedModel.name}-mitre-test-detailed-report.json`,
     `${besecureMlAssessmentDataStore}/${selectedModel.name}/llm-benchmark/${selectedModel.name}-frr-test-summary-report.json`,
     `${besecureMlAssessmentDataStore}/${selectedModel.name}/llm-benchmark/${selectedModel.name}-spear-phishing-test-summary-report.json`,
-    `${besecureMlAssessmentDataStore}/${selectedModel.name}/llm-benchmark/${selectedModel.name}-prompt-injection-test-summary-report.json`
+    `${besecureMlAssessmentDataStore}/${selectedModel.name}/llm-benchmark/${selectedModel.name}-prompt-injection-test-summary-report.json`,
+    `${besecureMlAssessmentDataStore}/${selectedModel.name}/llm-benchmark/${selectedModel.name}-prompt-injection-test-detailed-report.json`,
+    `${besecureMlAssessmentDataStore}/${selectedModel.name}/llm-benchmark/${selectedModel.name}-autocomplete-test-detailed-report.json`,
+    `${besecureMlAssessmentDataStore}/${selectedModel.name}/llm-benchmark/${selectedModel.name}-instruct-test-detailed-report.json`
   ];
   const [interpreterData, setInterpreterData] = useState<InterpreterData>({});
   const [autocompleteData, setAutocompleteData] = useState<AutocompleteData>({});
   const [instructData, setInstructData] = useState<InstructData>({});
   const [mitreData, setMitreData] = useState<MitreDataArray>([]);
+  const [promptInjectionData, setPromptInjectionData] = useState<PromptInjectionStats>({});
   const [frrData, setFrrData] = useState<FRRData>({});
   const [spearPhishingData, setSpearPhishingData] = useState<SpearPhishingStats>({});
-  const [promptInjectionData, setPromptInjectionData] = useState<PromptInjectionStats>({});
+  const [promptInjectionSummaryData, setPromptInjectionSummaryData] = useState<PromptInjectionStats>({});
+  const [autocompleteDetailedData, setAutocompleteDetailedData] = useState<AutocompleteDetailDataArray>([]);
+  const [instructTestDetailedData, setInstructTestDetailedData] = useState<AutocompleteDetailDataArray>([]);
+  const [openAutocomplete, setOpenAutocomplete] = useState(false);
+  const [openInstruct, setOpenInstruct] = useState(false);
   const [open, setOpen] = useState(false);
+  const [openPromptInjection, setOpenPromptInjection] = useState(false);
   const [openSpear, setOpenSpear] = useState(false);
   const handleOpen = () => setOpenSpear(true);
   const handleClose = () => setOpenSpear(false);
   const handleOpenMitre = () => setOpen(true);
+  const handleOpenPromptInjection = () => setOpenPromptInjection(true);
+  const handleOpenAutocomplete = () => setOpenAutocomplete(true);
+  const handleCloseAutocomplete = () => setOpenAutocomplete(false);
+  const handleOpenInstruct = () => setOpenInstruct(true);
+  const handleCloseInstruct = () => setOpenInstruct(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -239,7 +292,10 @@ const SummaryDashboard = ({ model }: any) => {
           verifyLink(urls[3], setMitreData, []),
           verifyLink(urls[4], setFrrData),
           verifyLink(urls[5], setSpearPhishingData),
-          verifyLink(urls[6], setPromptInjectionData)
+          verifyLink(urls[6], setPromptInjectionSummaryData),
+          verifyLink(urls[7], setPromptInjectionData, []),
+          verifyLink(urls[8], setAutocompleteDetailedData, []),
+          verifyLink(urls[9], setInstructTestDetailedData, [])
         ]);
       } catch (err) {
         // Fix me later
@@ -275,8 +331,8 @@ const SummaryDashboard = ({ model }: any) => {
   const spearPhishingNumber = spearPhishingData.model_stats?.persuasion_average ? spearPhishingData.model_stats.persuasion_average : 0;
 
   const promptInjectionresult = [
-    { name: "Successful", value: promptInjectionData?.stat_per_model?.injection_successful_count ?? 0, color: "#1f77b4" },
-    { name: "Unsuccessful", value: promptInjectionData?.stat_per_model?.injection_unsuccessful_count ?? 0, color: "#ff7f0e" },
+    { name: "Successful", value: promptInjectionSummaryData?.stat_per_model?.injection_successful_count ?? 0, color: "#1f77b4" },
+    { name: "Unsuccessful", value: promptInjectionSummaryData?.stat_per_model?.injection_unsuccessful_count ?? 0, color: "#ff7f0e" },
   ];
 
   return (
@@ -438,6 +494,85 @@ const SummaryDashboard = ({ model }: any) => {
               </ResponsiveContainer>
             ) }
           </CardContent>
+          <Grid pb={ 1 } pl={ 0.5 }>
+
+            <Button
+              variant="text"
+              onClick={ handleOpenAutocomplete }
+              disabled={ (Object.keys(autocompleteData).length === 0) && Object.keys(autocompleteDetailedData).length === 0 }
+              sx={ {
+                textTransform: 'none',
+                '&:hover': {
+                  backgroundColor: 'rgba(0, 0, 0, 0.1)',
+                  color: 'primary.main'
+                },
+                width: "fit-content",
+    
+              } }
+            >
+              Autocomplete Test Summary
+            </Button>
+            <Modal
+              aria-labelledby="transition-modal-title"
+              aria-describedby="transition-modal-description"
+              open={ openAutocomplete }
+              onClose={ handleCloseAutocomplete }
+              closeAfterTransition
+              slots={ { backdrop: Backdrop } }
+              slotProps={ {
+                backdrop: {
+                  timeout: 500,
+                },
+              } }
+            >
+              <Fade in={ openAutocomplete }>
+                <Box sx={ modalStyle }>
+                  <AutocompleteModal autocompleteSummaryData = { autocompleteData } autocompleteDetailedData = { autocompleteDetailedData } data = "Autocomplete"
+                  />
+ 
+  
+                </Box>
+              </Fade>
+            </Modal>
+            <Button
+              variant="text"
+              onClick={ handleOpenInstruct }
+              disabled={ (Object.keys(instructData).length === 0) && Object.keys(instructTestDetailedData).length === 0 }
+
+              sx={ {
+                textTransform: 'none',
+                '&:hover': {
+                  backgroundColor: 'rgba(0, 0, 0, 0.1)',
+                  color: 'primary.main'
+                },
+                width: "fit-content",
+    
+              } }
+            >
+              Instruct Test Summary
+            </Button>
+            <Modal
+              aria-labelledby="transition-modal-title"
+              aria-describedby="transition-modal-description"
+              open={ openInstruct }
+              onClose={ handleCloseInstruct }
+              closeAfterTransition
+              slots={ { backdrop: Backdrop } }
+              slotProps={ {
+                backdrop: {
+                  timeout: 500,
+                },
+              } }
+            >
+              <Fade in={ openInstruct }>
+                <Box sx={ modalStyle }>
+                  <AutocompleteModal autocompleteSummaryData = { instructData } autocompleteDetailedData = { instructTestDetailedData } data = "Instruct"
+                  />
+  
+                </Box>
+              </Fade>
+            </Modal>
+          </Grid> 
         </Card>
       </Grid>
 
@@ -629,36 +764,77 @@ const SummaryDashboard = ({ model }: any) => {
       </Grid>
 
       <Grid item xs={ 12 } md={ 12 } lg={ 3 }>
-        <Card sx={ { height: "100%", display: "flex", flexDirection: "column", alignItems: "center", textAlign: "center", padding: 2 } }>
-          <CardContent>
-            <Typography variant="h5" sx={ { fontWeight: 600, letterSpacing: 1, color: "gray", textTransform: "none" } }>
-              Prompt Injection
-            </Typography>
-            <Typography variant="subtitle2" sx={ { color: "gray" } }>
-              Model’s susceptibility to prompt injection attack scenarios
-            </Typography>
-            { promptInjectionresult[0].value === 0 && promptInjectionresult[1].value === 0 ? (
-              <Box sx={ { display: "flex", justifyContent: "center", alignItems: "center", height: 200 } }>
-                <Typography variant="body1" color="textSecondary">
-                  Data not available
-                </Typography>
-              </Box>
-            ) : (
-              <ResponsiveContainer width="100%" height={ 180 }>
-                <PieChart>
-                  <Pie data={ promptInjectionresult } dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={ 50 } label>
-                    { promptInjectionresult.map((entry, index) => (
-                      <Cell key={ `cell-${index}` } fill={ entry.color } />
-                    )) }
-                  </Pie>
-                  <Tooltip />
-                  <Legend wrapperStyle={ { fontSize: '12px' } } />
-                </PieChart>
-              </ResponsiveContainer>
-            ) }
-          </CardContent>
-        </Card>
+        <>
+          <Button
+            onClick={ handleOpenPromptInjection }
+            variant="contained"
+            sx={ {
+              height: '100%',
+              width: '100%',
+              paddingTop: 2,
+              paddingLeft: 0,
+              paddingRight: 0
+            } }
+            style={ { backgroundColor: 'white' } }
+          >
+            <CardContent>
+              <Typography variant="h5" sx={ { fontWeight: 600, letterSpacing: 1, color: "gray", textTransform: "none" } }>
+                Prompt Injection
+              </Typography>
+              <Typography variant="subtitle2" sx={ { color: "gray" } }>
+                Model’s susceptibility to prompt injection attack scenarios
+              </Typography>
+              { promptInjectionresult[0].value === 0 && promptInjectionresult[1].value === 0 ? (
+                <Box sx={ { display: "flex", justifyContent: "center", alignItems: "center", height: 200 } }>
+                  <Typography variant="body1" color="textSecondary">
+                    Data not available
+                  </Typography>
+                </Box>
+              ) : (
+                <ResponsiveContainer width="100%" height={ 180 }>
+                  <PieChart>
+                    <Pie data={ promptInjectionresult } dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={ 50 } label>
+                      { promptInjectionresult.map((entry, index) => (
+                        <Cell key={ `cell-${index}` } fill={ entry.color } />
+                      )) }
+                    </Pie>
+                    <Tooltip />
+                    <Legend wrapperStyle={ { fontSize: '12px' } } />
+                  </PieChart>
+                </ResponsiveContainer>
+              ) }
+            </CardContent>
+          
+          </Button>
 
+          <Modal
+            open={ openPromptInjection }
+            onClose={ () => setOpenPromptInjection(false) }
+            closeAfterTransition
+            slots={ { backdrop: Backdrop } }
+            slotProps={ { backdrop: { timeout: 500 } } }
+          >
+            <Fade in={ openPromptInjection }>
+              <Box
+                sx={ {
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  width: "95%",
+                  maxHeight: "90vh",
+                  overflowY: "auto",
+                  bgcolor: "#f4f4f4",
+                  boxShadow: 24,
+                  p: 4,
+                  borderRadius: 2,
+                } }
+              >
+                <PromptInjectionModal promptInjectionData={ promptInjectionData } promptInjectionSummaryData={ promptInjectionSummaryData } />
+              </Box>
+            </Fade>
+          </Modal>
+        </>
       </Grid>
     </Grid>
   );
