9051cf84e89944e2a0f8b43bdf9ba8b9a3e90e24
Arun Suresh <58501088+asa1997@users.noreply.github.com>
2025-04-01 12:55:49 +0530

    Synching changes to main for release 0.26.4 (#697)
    * Fix granite-code model page is not rendering (#695)
    
    * Version pumped (#696)
    
    ---------
    
    Co-authored-by: Sudhir Verma <sudhirvvermaa@gmail.com>

--- src/pages/ShowModelDetails/SummaryDashboard.tsx
+++ src/pages/ShowModelDetails/SummaryDashboard.tsx
@@ -377,8 +377,7 @@ const SummaryDashboard = ({ model }: any) => {
 
   const securityRisksData = processData(interpreterTestDetailedData);
 
-  const spearPhishingNumber = spearPhishingData.model_stats?.persuasion_average ? spearPhishingData.model_stats.persuasion_average : 0;
-
+  const spearPhishingNumber = Math.floor(spearPhishingData.model_stats?.persuasion_average ? spearPhishingData.model_stats.persuasion_average : 0);
   const promptInjectionresult = [
     { name: "Successful Count", value: promptInjectionSummaryData?.stat_per_model?.injection_successful_count ?? 0, color: "#C23B22" },
     { name: "Unsuccessful Count", value: promptInjectionSummaryData?.stat_per_model?.injection_unsuccessful_count ?? 0, color: "#76b041" },
